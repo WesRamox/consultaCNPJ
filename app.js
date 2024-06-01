1991f4a3c26d5f99bddb5c4fcf6dc12b908517b9
WesRamox <wesleyramox258@gmail.com>
2024-06-01 13:27:12 -0300

    Update

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const cpnjInput = document.querySelector('#cnpj')
 const sendButton = document.querySelector('.search-button')
 
-const createResult = () => {
-   
+const createResult = (data) => {
+   const listDetails = document.querySelector('.response')
+   const companyName = document.createElement("h1")
+   const companyCnpj = document.createElement("p")
+   const companyCeo = document.createElement("p")
+   // const companyCapital = document.createElement("h")
+
+   companyName.textContent = `${data.razao_social}`
+   companyName.classList.add('text-xl')
+   companyCnpj.textContent = `CNPJ: ${data.cnpj}`
+   companyCeo.textContent = `Sócio(a): ${data.qsa[0].nome_socio}`
+   // companyCapital.textContent = 
+
+   listDetails.append(companyName, companyCeo, companyCnpj)
 }
 
 
@@ -11,7 +23,7 @@ async function searchCNPJ() {
    const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`)
    const data = await response.json()
 
-   console.log(data)
+   await createResult(data)
 }
 
 sendButton.addEventListener('click', (ev) => {
